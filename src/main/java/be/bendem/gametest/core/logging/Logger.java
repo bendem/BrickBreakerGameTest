3e28769a03a2547ab8fabf5b52706f063fa6008f
bendem <online@bendem.be>
2014-09-25 12:48:30 +0200

    Added time to logs

--- src/main/java/be/bendem/gametest/core/logging/Logger.java
+++ src/main/java/be/bendem/gametest/core/logging/Logger.java
@@ -1,11 +1,15 @@
 package be.bendem.gametest.core.logging;
 
 import java.io.PrintStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * @author bendem
  */
 public class Logger {
+
+    private static final SimpleDateFormat TIME_FORMAT = new SimpleDateFormat("HH:mm:ss SSS");
 
     public static void debug(String string) {
         debug(string, null);
@@ -41,10 +45,14 @@ public class Logger {
 
     private static void log(String string, Throwable throwable, LogLevel level) {
         PrintStream out = level.isError() ? System.err : System.out;
-        out.println("[" + level.getName() + "] " + string);
+        out.println("[" + level.getName() + "] [" + getTime() + "] " + string);
         if(throwable != null) {
             throwable.printStackTrace(out);
         }
+    }
+
+    private static String getTime() {
+        return TIME_FORMAT.format(new Date());
     }
 
     private enum LogLevel {
